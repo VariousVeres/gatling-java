f9a498f454e79f9592ab63fb289043137f50a2e5
oleksandr.veresiuk <oleksandr.veresiuk@eleks.com>
2023-08-17 19:09:05 +0300

    blackwork_commit

---
+++ target/gatling/basicsimulation-20230817160815460/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1692288496000,1],[1692288497000,1],[1692288498000,1],[1692288499000,1],[1692288500000,1],[1692288501000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
